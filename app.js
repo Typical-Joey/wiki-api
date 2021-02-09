bf59b8c956391a9ae3c086203b7cafbbefddc356
Typical-Joey <jtcapocci@yahoo.com>
2021-02-08 20:35:44 -0600

    Add new articles to database

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const ejs = require("ejs");
 const mongoose = require("mongoose");
 
 const app = express();
@@ -15,10 +16,44 @@ mongoose.connect("mongodb://localhost:27017/wikiDB", {
     useFindAndModify: false
 });
 
-
-app.get("/", function (req, res) {
-    res.send("Server is running");
+const articleSchema = mongoose.Schema({
+    title: String,
+    content: String
 });
+
+const Article = mongoose.model("Article", articleSchema);
+
+// Find all articles in database
+app.get("/articles", function (req, res) {
+    Article.find(function (err, articles) {
+        if (!err) {
+            res.send(articles);
+        } else {
+            res.send(err);
+        }
+    })
+});
+
+// Add new articles
+app.post("/articles", function (req, res) {
+    const title = req.body.title;
+    const content = req.body.content;
+
+    const article = new Article({
+        title: title,
+        content: content
+    });
+
+    article.save(function (err) {
+        if (err) {
+            res.send(err);
+        } else {
+            res.send("Succesfully added new article!");
+        }
+    });
+
+});
+
 
 
 app.listen(process.env.PORT || 3000, function () {
